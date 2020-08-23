43ffc284be9ea8e4025016f786e432c34075a079
gabrielmougard <gabriel.mougard@gmail.com>
2020-08-23 11:59:30 +0200

    patch django project naming

--- 1interviewparjour-api/one_interviewparjour/asgi.py
+++ 1interviewparjour-api/one_interviewparjour/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for one_interviewparjour_backend project.
+ASGI config for one_interviewparjour project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_interviewparjour_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_interviewparjour.settings')
 
 application = get_asgi_application()
--- 1interviewparjour-api/one_interviewparjour/urls.py
+++ 1interviewparjour-api/one_interviewparjour/urls.py
@@ -1,4 +1,4 @@
-"""one_interviewparjour_backend URL Configuration
+"""one_interviewparjour URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
