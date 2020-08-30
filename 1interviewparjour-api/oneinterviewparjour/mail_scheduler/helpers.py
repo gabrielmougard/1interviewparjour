34bccf188fbbe68edd9707e45e8adef54f8c06d2
gabrielmougard <gabriel.mougard@gmail.com>
2020-08-30 22:46:07 +0200

    WIP on mail_scheduler.helpers.generate_mail_template2

--- 1interviewparjour-api/oneinterviewparjour/mail_scheduler/helpers.py
+++ 1interviewparjour-api/oneinterviewparjour/mail_scheduler/helpers.py
@@ -1,3 +1,48 @@
+import re
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+from django.template.loader import get_template
+from django.template import Context
+
+
+def insert_line_numbers(html):
+    match = re.search('(<pre[^>]*>)(.*)(</pre>)', html, re.DOTALL)
+    if not match: return html
+
+    pre_open = match.group(1)
+    pre = match.group(2)
+    pre_close = match.group(3)
+
+    html = html.replace(pre_close, '</pre></td></tr></table>')
+    numbers = range(1, pre.count('\n') + 1)
+    format = '%' + str(len(str(numbers[-1]))) + 'i'
+    lines = '\n'.join(format % i for i in numbers)
+    html = html.replace(pre_open, '<table><tr><td>' + pre_open + lines + '</pre></td><td>' + pre_open)
+    return html
+
+
+def _to_HTML_code(code, lexer, options, style, linenos, divstyles): # in our configuration, divstyles is `padding:.2em .6em;` and style is 'default'
+    lexer = lexer or 'python'
+    style = style or 'default'
+    defstyles = 'overflow:auto;width:auto;'
+
+    formatter = HtmlFormatter(
+        style=style,
+        linenos=False,
+        noclasses=True,
+        cssclass='',
+        cssstyles=defstyles + divstyles,
+        prestyles='margin: 0'
+    )
+    html = highlight(code, get_lexer_by_name(lexer, **options), formatter)
+    if linenos:
+        html = insert_line_numbers(html)
+    return html
+
+
 def generate_template_mail(problem_metadata, pro):
     """
     1) Use a mardown to html generator for the `problem_metadata['exercise_body']`
@@ -40,8 +85,30 @@ def generate_template_mail(problem_metadata, pro):
         exercise_body_formatted += "\n"
     #
     # 2)
+    exercise_solution_formatted = ""
     if not pro:
-        
+        exercise_solution_formatted = "\n".join(problem_metadata['exercise_solution'].split("\n")[:12])
+    else:
+        exercise_solution_formatted = problem_metadata['exercise_solution']
+    #
+    # 3)
+    exercise_bootcode_formatted = _to_HTML_code(problem_metadata['exercise_bootcode'], 'python', {}, 'default', False, 'padding:.2em .6em;')
+    exercise_solution_formatted = _to_HTML_code(exercise_solution_formatted, 'python', {}, 'default', False, 'padding:.2em .6em;')
+    #
+    # 4)
+    if pro:
+        htmlpro = get_template('mail-pro.html')
+        if problem_metadata['company']['name'] == "1interviewparjour":
+            # TODO
+        else:
+            # TODO
+    else:
+        htmlnotpro = get_template('mail-not-pro.html')
+        if problem_metadata['company']['name'] == "1interviewparjour":
+            # TODO
+        else:
+            # TODO
+
 
 
 def _convert_to_strings(list_of_strs):
