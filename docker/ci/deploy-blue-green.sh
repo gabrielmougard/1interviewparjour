16735a3c8e16556f1c72ac8b31f1c1b238e46c96
gabrielmougard <gabriel.mougard@gmail.com>
2020-10-15 17:17:32 +0200

    integration front/back v0.12

--- docker/ci/deploy-blue-green.sh
+++ docker/ci/deploy-blue-green.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ $(docker ps -f name=ci_1interviewparjour-backend-blue -q) ]
+if [ $(docker ps -f name=1interviewparjour-backend-blue -q) ]
 then
-    ENV_BACK="ci_1interviewparjour-backend-green"
-    OLD_BACK="ci_1interviewparjour-backend-blue"
+    ENV_BACK="1interviewparjour-backend-green"
+    OLD_BACK="1interviewparjour-backend-blue"
 else
-    ENV_BACK="ci_1interviewparjour-backend-blue"
-    OLD_BACK="ci_1interviewparjour-backend-green"
+    ENV_BACK="1interviewparjour-backend-blue"
+    OLD_BACK="1interviewparjour-backend-green"
 fi
 
 echo "Starting "$ENV_BACK" container"
@@ -19,7 +19,7 @@ echo "Stopping "$OLD_BACK" container"
 docker-compose --project-name=$OLD_BACK stop
 
 # frontend configuration
-if [ $ENV_BACK == "ci_1interviewparjour-backend-green" ]; then
+if [ $ENV_BACK == "1interviewparjour-backend-green" ]; then
     BACKEND_ENDPOINT="http://localhost:8001"
     echo "The backend endpoint (green mode) is $BACKEND_ENDPOINT"
 else
@@ -29,13 +29,13 @@ fi
 sed -e "s/\${backend-endpoint}/$BACKEND_ENDPOINT/" web.env > web-temp.env && rm web.env && cat web-temp.env > web.env && rm web-temp.env
 #
 
-if [ $(docker ps -f name=ci_1interviewparjour-web-blue -q) ]
+if [ $(docker ps -f name=1interviewparjour-web-blue -q) ]
 then
-    ENV_FRONT="ci_1interviewparjour-web-green"
-    OLD_FRONT="ci_1interviewparjour-web-blue"
+    ENV_FRONT="1interviewparjour-web-green"
+    OLD_FRONT="1interviewparjour-web-blue"
 else
-    ENV_FRONT="ci_1interviewparjour-web-blue"
-    OLD_FRONT="ci_1interviewparjour-web-green"
+    ENV_FRONT="1interviewparjour-web-blue"
+    OLD_FRONT="1interviewparjour-web-green"
 fi
 
 echo "Starting "$ENV_FRONT" container"
