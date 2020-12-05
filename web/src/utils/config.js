9e244edf2d41f47fc442356b7b52342cd54b5162
gabrielmougard <gabriel.mougard@gmail.com>
2020-12-05 19:23:23 +0100

    [FRONT] change api endpoints

--- web/src/utils/config.js
+++ web/src/utils/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    API_URL: process.env.BACKEND_ENDPOINT || 'http://localhost:8000',
+    API_URL: process.env.API_URL || 'http://localhost:8000',
     ROOT_URL: process.env.ROOT_URL || 'http://localhost:3000',
   }
