96eb58c60594da42c16eb10b7782e77c65370d50
Gabriel Mougard <gabriel.mougard@gmail.com>
2021-01-07 00:27:04 +0100

    [CI/CD] Rolling update features - all in one script + Jenkins pipeline update

--- docker/ci/update-stack.sh
+++ docker/ci/update-stack.sh
@@ -4,20 +4,37 @@ export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 1
 export VAULT_ADDR=http://127.0.0.1:8200
 sed -e 's/\${dockerhost}/'"${DOCKERHOST}"'/' backend.env > backend-temp.env && rm backend.env && cat backend-temp.env > backend.env && rm backend-temp.env
 
-# Required : the docker swarm '1interviewparjour-swarm' is already up and running
-# Update the application stack without shuting it down
-docker service update -d \
-    --image gabrielmougard/1interviewparjour-web:latest \
-    --update-parallelism 1 \
-    --update-failure-action rollback \
-    --update-order start-first \
-    --update-delay 30s \
-    1interviewparjour-swarm_1interviewparjour-web
-docker service update -d \
-    --image gabrielmougard/1interviewparjour-app:latest \
-    --update-parallelism 1 \
-    --update-failure-action rollback \
-    --update-order start-first \
-    --update-delay 30s \
-    1interviewparjour-swarm_1interviewparjour
+
+SWARM_NAME="1interviewparjour-swarm"
+SWARM_CREATED=1
+declare -a ServiceNameSuffixes=("1interviewparjour-app" "1interviewparjour-web" "prometheus" "node-exporter" "granfana" )
+declare -a AppStackServices=("1interviewparjour-app" "1interviewparjour-web")
+
+# Iterate the string array using for loop
+for val in ${ServiceNameSuffixes[@]}; do
+    SERVICE_REACHABLE=$(docker service ps ${SWARM_NAME}_${val})
+    if [[ $SERVICE_REACHABLE -eq "no such service: ${SWARM_NAME}_${val}" ]]; then
+        SWARM_CREATED=0
+        break
+    fi
+done
+
+if [[$SWARM_CREATED -eq 0]]
+then
+    # The swarm is not created yet. Create it.
+    docker stack deploy -d --compose-file 1interviewparjour-stack.yml 1interviewparjour-swarm
+else
+    # The swarm is created (all the services are up and running).
+    # Then, we just update the application stack without shuting it down
+    for val in ${AppStackServices[@]}; do
+        docker service update -d \
+        --image gabrielmougard/${val}:latest \
+        --update-parallelism 1 \
+        --update-failure-action rollback \
+        --update-order start-first \
+        --update-delay 30s \
+        ${SWARM_NAME}_${val}
+    done
+fi
+
 
