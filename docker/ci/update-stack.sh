fb876b7f0b0cc8a8e8787f0859bbeace66ac9fbd
Gabriel Mougard <gabriel.mougard@gmail.com>
2021-01-07 15:46:23 +0100

    [CI/CD] fix in update-stack.sh #2

--- docker/ci/update-stack.sh
+++ docker/ci/update-stack.sh
@@ -8,22 +8,23 @@ cd ..
 
 SWARM_NAME="1interviewparjour-swarm"
 SWARM_CREATED=1
-declare -a ServiceNameSuffixes=("1interviewparjour-app" "1interviewparjour-web" "prometheus" "node-exporter" "granfana" )
+declare -a ServiceNameSuffixes=("1interviewparjour-app" "1interviewparjour-web" "prometheus" "node-exporter" "granfana")
 declare -a AppStackServices=("1interviewparjour-app" "1interviewparjour-web")
 
 # Iterate the string array using for loop
 for val in ${ServiceNameSuffixes[@]}; do
-    SERVICE_REACHABLE=$(docker service ps ${SWARM_NAME}_${val})
-    if [[ $SERVICE_REACHABLE -eq "no such service: ${SWARM_NAME}_${val}" ]]; then
+    REACHABLE=$(docker service ps ${SWARM_NAME}_${val} 2>&1)
+    if [ "$REACHABLE" == "no such service: ${SWARM_NAME}_${val}" ]; then
         SWARM_CREATED=0
         break
     fi
 done
 
-if [[$SWARM_CREATED -eq 0]]
+if [[ "$SWARM_CREATED" -eq 0 ]]
 then
     # The swarm is not created yet. Create it.
-    docker stack deploy -d --compose-file 1interviewparjour-stack.yml 1interviewparjour-swarm
+    echo "Creating the swarm ..."
+    docker stack deploy --compose-file 1interviewparjour-stack.yml 1interviewparjour-swarm
 else
     # The swarm is created (all the services are up and running).
     # Then, we just update the application stack without shuting it down
