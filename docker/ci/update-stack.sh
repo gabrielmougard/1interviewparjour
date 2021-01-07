66636f3cae0c94645a94fb738fb7d421c01908c4
Gabriel Mougard <gabriel.mougard@gmail.com>
2021-01-07 15:48:47 +0100

    [CI/CD] fix in update-stack.sh #3

--- docker/ci/update-stack.sh
+++ docker/ci/update-stack.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 export VAULT_ADDR=http://127.0.0.1:8200
